41411abed2211b38ba870e8d862e08ad6d368ded
Orlanto2100 <percyorlanto2100@gmail.com>
2026-09-03 16:09:08 +0630

    Implemented Customer module

---
+++ Inventory Management System/src/main/java/com/percy/inventory/Customer/dto/CreateCustomerRequest.java
@@ -0,0 +1,17 @@
+package com.percy.inventory.Customer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCustomerRequest {
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+}
